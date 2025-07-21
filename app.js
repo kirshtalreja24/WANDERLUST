6ea9e0f09b7a6f98a11be1fa4dac7dfc1489d25d
kirshtalreja24 <161924216+kirshtalreja24@users.noreply.github.com>
2025-07-21 16:58:37 +0500

    .

--- app.js
+++ app.js
@@ -3,42 +3,86 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const MONGO_URL ='mongodb://127.0.0.1:27017/wanderlust'
-
-
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 main()
-.then((res) => {
-    console.log("Connected to DB ");
-})
-.catch((err) => {
-    console.log(err);
-});
+    .then((res) => {
+        console.log("Connected to DB ");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 }
 
-app.set("views engine" , "ejs");
-app.set("views" , path.join(__dirname , "/views"));
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'))
 
+app.engine("ejs" , ejsMate);
+app.use(express.static(path.join(__dirname , "/public")));
 
-
-
-app.get("/" , (req,res) => {
+app.get("/", (req, res) => {
     res.send("Hi, I am root");
 })
 
 //Index Route
-app.get("/listings" , async (req,res) => {
-    const all_listings =   await Listing.find({});
-    res.render("listings/index.ejs" , {all_listings});
+app.get("/listings", async (req, res) => {
+    const all_listings = await Listing.find({});
+    res.render("listings/index.ejs", { all_listings });
+})
+
+//New  Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const list = await Listing.findById(id);
+    res.render("listings/show.ejs", { list });
+})
+
+// Create Route
+app.post("/listings", async (req, res) => {
+    // let listing = req.body.listing;
+    const newList = new Listing(req.body.listing);
+    await newList.save()
+    res.redirect("/listings");
+})
+
+//Edit Route
+app.get("/listings/:id/edit" , async (req,res) => {
+    let { id } = req.params;
+    const list = await Listing.findById(id);
+    res.render("listings/edit.ejs" , {list});
+})
+
+//Update Route
+app.put("/listings/:id" , async (req,res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id , {...req.body.listing} , {runValidators:true}); 
+
+     res.redirect(`/listings/${id}`)
+})
+
+//Destroy Route
+app.delete("/listings/:id" , async (req,res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    
+    res.redirect("/listings");
 })
 
 
 
-
-
-app.listen(8080, ()=>{
+app.listen(8080, () => {
     console.log("server is listening to port 8080");
 })
--- models/listing.js
+++ models/listing.js
@@ -9,18 +9,42 @@ const listingSchema = new Schema({
     description:{
         type:String
     },
-    image:{
-        type:String,
-        default:"https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+   image: {
+    type: {
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1518684079-3c830dcef090?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZHViYWl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+        }
     },
+    default: function () {
+        return {
+            filename: "listingimage",
+            url: "https://images.unsplash.com/photo-1518684079-3c830dcef090?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZHViYWl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+        };
+    },
+    set: (v) => {
+        // If empty string or falsy value is provided, fall back to default
+        if (!v || !v.url) {
+            return {
+                filename: "listingimage",
+                url: "https://images.unsplash.com/photo-1518684079-3c830dcef090?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZHViYWl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+            };
+        }
+        return v;
+    }
+},
+
     price:{
         type:Number
     },
     location:{
         type:String
     }, 
-    countyr:{
+    country:{
         type:String
     },
 });
