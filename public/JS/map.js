9109edfde8c6bba5f7d85e6b816c730836927daa
kirshtalreja24 <161924216+kirshtalreja24@users.noreply.github.com>
2025-07-30 15:32:40 +0500

    .

--- public/JS/map.js
+++ public/JS/map.js
@@ -1,11 +1,26 @@
 mapboxgl.accessToken = mapToken
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 8 // starting zoom
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 10 // starting zoom
 });
 
+const popupOffsets = {
+    'top': [0, 25],
+    'bottom': [0, -25],
+    'left': [25, 0],
+    'right': [-25, 0]
+};
+
+
 // console.log(coordinates);
-const marker = new mapboxgl.Marker({color:"red"})
-    .setLngLat(coordinates)
+const popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
+    .setHTML(`
+        <h4> ${listing.location} </h4>
+        <p> Exact location provided after booking </p>`);
+
+// Create the marker
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(popup) 
     .addTo(map);
